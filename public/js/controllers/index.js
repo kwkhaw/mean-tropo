e33622b8ad18fa26424500b969df161ebb9453ee
kkw <kwkhaw@gmail.com>
2013-11-10 20:20:29 +0800

    experiment socket.io on client

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,3 +1,8 @@
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
+angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'btford.socket-io', function ($scope, Global, socket) {
     $scope.global = Global;
+    socket.forward('sms', $scope);
+    scope.$on('socket:sms', function (ev, data) {
+      $scope.theData = data;
+      console.log($scope.theData.text);
+    });
 }]);
