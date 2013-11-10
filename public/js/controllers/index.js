54ba208ae2de3f9a36386a59b7b4a402af7fbf83
kkw <kwkhaw@gmail.com>
2013-11-10 21:19:29 +0800

    fixed missing $

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,7 +1,7 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'btford.socket-io', function ($scope, Global, socket) {
     $scope.global = Global;
     socket.forward('sms', $scope);
-    scope.$on('socket:sms', function (ev, data) {
+    $scope.$on('socket:sms', function (ev, data) {
       $scope.theData = data;
       console.log($scope.theData.text);
     });
