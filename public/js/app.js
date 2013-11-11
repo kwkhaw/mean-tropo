5a2c7d8427a803cbcbeb93f16b1e372eb1f6641a
kkw <kwkhaw@gmail.com>
2013-11-11 22:01:39 +0800

    test

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,13 @@
-window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.consumptions', 'btford.socket-io']);
+window.app = angular.module('mean', [
+              'ngCookies',
+              'ngResource',
+              'ui.bootstrap',
+              'ui.route',
+              'mean.system',
+              'mean.articles',
+              'mean.consumptions',
+              'btford.socket-io']);
 
-angular.module('mean.system', []);
+angular.module('mean.system', ['btford.socket-io']);
 angular.module('mean.articles', []);
 angular.module('mean.consumptions', []);
